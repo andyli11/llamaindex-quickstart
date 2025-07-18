ae07acfca896db64a23ac58bee5afe7a5c9fcdf4
Andy Li <andy.li@uwaterloo.ca>
2025-07-18 11:27:16 -0400

    initial commit

---
+++ src/multi_agent_assistant/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from llamaindex-quickstart!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/multi_agent_assistant/multi_agent_assistant.py
@@ -0,0 +1,4 @@
+from llama_index.llms.google_genai import GoogleGenAI
+from llama_index.llms.google_genai import GoogleGenAI
+
+llm = GoogleGenAI(model="gemini-2.5-pro")
